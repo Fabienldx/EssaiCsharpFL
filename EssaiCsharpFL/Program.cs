7284b1d3776182efdef44b9602c7dab2940fe906
fledoux <fledoux@N112-14>
2023-09-12 08:42:50 +0200

    Ajoutez des fichiers projet.

---
+++ EssaiCsharpFL/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssaiCsharpFL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quel est votre nom ?");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Quel est votre prénom ?");
+            string prenom = Console.ReadLine();
+            Console.WriteLine("Quel est votre âge ?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Votre nom et prénom sont {0} {1}, et vous avez {2} ans", nom, prenom, age);
+            Console.ReadKey();
+        }
+    }
+}
